a06501eb31256701fe86928d986787db21a4f88c
Dionatan <dionatanonliveira@cidadaoon.com>
2023-07-03 16:47:43 -0300

    finalizando rotas e testes

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,10 +1,10 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
 
+process.env.NODE_ENV === 'test' ? config({ path: '.env.test' }) : config()
+
 const envSchema = z.object({
-  NODE_ENV: z
-    .enum(['development', 'teste', 'production'])
-    .default('production'),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
